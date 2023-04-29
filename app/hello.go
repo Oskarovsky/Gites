c473dfb92fc01c4e8fd226c89ed9e95e965d2323
Oskarovsky <oskar.slyk@vodeno.com>
2023-04-29 17:46:59 +0200

    Git commit manual

--- app/hello.go
+++ app/hello.go
@@ -6,5 +6,28 @@ func main() {
 	fmt.Println("Oskar init git")
 	var x = []int{10, 3: 20, 30}
 
-	fmt.Printf("Test slices: %v", x)
+	fmt.Printf("Test slices: %v\n", x)
+
+	var turbo []int
+	turbo = append(turbo, 2)
+	turbo = append(turbo, 10)
+	fmt.Printf("Turbo: %v", turbo)
+
+	fmt.Printf("\n=====\n")
+	fmt.Println(turbo, len(turbo), cap(turbo))
+	turbo = append(turbo, 10)
+
+	fmt.Println(turbo, len(turbo), cap(turbo))
+	turbo = append(turbo, 20)
+
+	fmt.Println(turbo, len(turbo), cap(turbo))
+	turbo = append(turbo, 40)
+
+	fmt.Println(turbo, len(turbo), cap(turbo))
+	turbo = append(turbo, 50)
+
+	fmt.Println(turbo, len(turbo), cap(turbo))
+	turbo = append(turbo, 60)
+
+	fmt.Printf("\n")
 }
